de153141910dcde234bc383bab326eec21db2e75
Ritwik <ritwikhero@gmail.com>
2023-05-10 14:43:11 +0530

    Encapusaltion assingment solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//C:\Users\ritwi\IdeaProjects\encapsulation-ritwikhero\src\main\java\com\driver\Main.java:6:12
+//java: name has private access in com.driver.RWOnly
+//        obj.name = "Ritwik";
+
+        obj.setName("Ritwik");
+        System.out.println(obj.getName());
+    }
 }
